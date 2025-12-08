5dc92bf0058c211f0f2ffca3e7f47d71de0862ea
Diwice <coldwice@gmail.com>
2025-12-08 17:32:18 +0300

    CH4L4

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -29,7 +29,18 @@ func main() {
 	pause_handler := handlerPause(state)
 	exchange, queue_name, queue_type := routing.ExchangePerilDirect, fmt.Sprintf("%v.%v", routing.PauseKey, c_name), pubsub.QueueTypeTransient
 	if err := pubsub.SubscribeJSON(conn, exchange, queue_name, routing.PauseKey, queue_type, pause_handler); err != nil {
-		log.Fatal("Couldn't subscribe to new message queue:", err)
+		log.Fatal("Couldn't subscribe to the pause queue:", err)
+	}
+
+	move_handler := handlerMove(state)
+	move_exch, move_q_name, move_key, move_q_type := routing.ExchangePerilTopic, fmt.Sprintf("%v.%v", "army_moves", c_name), "army_moves.*", pubsub.QueueTypeTransient
+	if err := pubsub.SubscribeJSON(conn, move_exch, move_q_name, move_key, move_q_type, move_handler); err != nil {
+		log.Fatal("Couldn't subscribe to the move queue:", err)
+	}
+
+	new_ch, err := conn.Channel()
+	if err != nil {
+		log.Fatal("Couldn't create a move channel:", err)
 	}
 
 	for {
@@ -39,9 +50,16 @@ func main() {
 				fmt.Println(err)
 			}
 		} else if inp[0] == "move" {
-			if _, err := state.CommandMove(inp); err != nil {
+			move, err := state.CommandMove(inp)
+			if err != nil {
 				fmt.Println(err)
 			}
+
+			if err = pubsub.PublishJSON(new_ch, move_exch, move_q_name, move); err != nil {
+				fmt.Println(err)
+			}
+
+			fmt.Println("Moved successfully!")
 		} else if inp[0] == "status" {
 			state.CommandStatus()
 		} else if inp[0] == "help" {
